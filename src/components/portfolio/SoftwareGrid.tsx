bd1c75aff1da49bf5b5ab309206df7b7385a1e32
Loxed <leopoldrombaut.org>
2025-10-11 22:39:45 +0200

    Update SoftwareGrid component for improved grid layout responsiveness

--- src/components/portfolio/SoftwareGrid.tsx
+++ src/components/portfolio/SoftwareGrid.tsx
@@ -47,7 +47,7 @@ export const SoftwareGrid: React.FC = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
             <div
               key={project.id}
